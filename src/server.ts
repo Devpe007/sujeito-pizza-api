c8fbb1415fb9981a635d1e822ee4f20cd2508902
Devpe007 <anddev007pe@gmail.com>
2022-05-27 18:34:16 -0300

    making the images available to the front-end

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import path from 'path';
 
 import { middlewareError } from './middlewares/error/Error';
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(middlewareError);
 
