397306087d6eb4c6f664da4ee06755c74154337a
Devpe007 <anddev007pe@gmail.com>
2022-05-26 22:49:31 -0300

    create AuthUserController controllers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../controllers/CreateUserController';
+import { AuthUserController } from '../controllers/AuthUserController';
 
 const router = Router();
+
+router.post('/session', new AuthUserController().handle);
 
 router.post('/users', new CreateUserController().handle);
 
