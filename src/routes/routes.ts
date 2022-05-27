ef6e72e9ba05f548d68bd0e7b13a5a01010b8a3c
Devpe007 <anddev007pe@gmail.com>
2022-05-26 23:45:08 -0300

    create DetailUserController controllers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
-import { CreateUserController } from '../controllers/CreateUserController';
-import { AuthUserController } from '../controllers/AuthUserController';
+import { CreateUserController } from '../controllers/user/CreateUserController';
+import { AuthUserController } from '../controllers/user/AuthUserController';
+import { DetailUserController } from '../controllers/user/DetailUserController';
+
+import { isAuthenticated } from '../middlewares/authentication/isAuthenticated';
 
 const router = Router();
+
+router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
 
