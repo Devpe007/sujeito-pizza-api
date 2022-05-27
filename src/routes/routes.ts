8896e16b0c74886bc4c08fa1b7f6b38888f26468
Devpe007 <anddev007pe@gmail.com>
2022-05-27 18:28:43 -0300

    create CreateProductController controllers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/multer';
 
 import { CreateUserController } from '../controllers/user/CreateUserController';
 import { AuthUserController } from '../controllers/user/AuthUserController';
@@ -7,9 +10,13 @@ import { DetailUserController } from '../controllers/user/DetailUserController';
 import { CreateCategoryController } from '../controllers/category/CreateCategoryController';
 import { ListCategoryController } from '../controllers/category/ListCategoryController';
 
+import { CreateProductController } from '../controllers/product/CreateProductController';
+
 import { isAuthenticated } from '../middlewares/authentication/isAuthenticated';
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload('./tmp'));
 
 // -- ROTAS USER --
 
@@ -24,5 +31,9 @@ router.post('/users', new CreateUserController().handle);
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+// -- ROTAS PRODUCT --
+
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
 
 export { router };
