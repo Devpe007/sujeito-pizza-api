a3c2dc3fb3a670993123e3f3582f946473be07ed
Devpe007 <anddev007pe@gmail.com>
2022-05-27 00:05:36 -0300

    create CreateCategoryController controllers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,14 +4,22 @@ import { CreateUserController } from '../controllers/user/CreateUserController';
 import { AuthUserController } from '../controllers/user/AuthUserController';
 import { DetailUserController } from '../controllers/user/DetailUserController';
 
+import { CreateCategoryController } from '../controllers/category/CreateCategoryController';
+
 import { isAuthenticated } from '../middlewares/authentication/isAuthenticated';
 
 const router = Router();
+
+// -- ROTAS USER --
 
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
 
 router.post('/users', new CreateUserController().handle);
+
+// -- ROTAS CATEGORY --
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
